392624c59417730ee26afd2da31c6700e8eb5be5
chabiaknt <chabiaknt@gmail.com>
2023-06-07 13:17:05 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
+import { Box, Button, Heading,Text } from '@chakra-ui/react';
+
+
+
 
 function App() {
+  const [data,setData]=useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='homepage' style={{textAlign:'center'}}> 
+     <Heading size='lg' m='20px'p='20px'>Welcome to Favorite NPM Packages</Heading>
+     
+     <Box border='4px' borderColor="brown" width='70%' mx='15rem' height='25rem'>
+     <Text my='10rem'>Right now there is no Favorite for you yet, please add.</Text>
+      <Button colorScheme='teal' size='lg'my='-2rem'>Add Fav</Button>
+     </Box>
+     <div>
+     
+     </div>
+   </div>
   );
 }
 
